e106f3cd57694d86286df337950af720edb20916
Vitkor Tatarnikov <viktor@tatarnikov.org>
2016-04-14 15:14:18 +0600

    Добавил многопоточность транскодирования

--- Yator.py
+++ Yator.py
@@ -14,7 +14,8 @@ import string
 from pyItunes import *
 
 from transliterate import translit
-
+from Queue import Queue
+from threading import Thread
 from Interfaces.Config import Config
 from Interfaces.Transcode import AudioTranscode
 
@@ -24,16 +25,24 @@ class Yator:
 
     def __init__(self):
         self.config = Config()
-        transcode = AudioTranscode()
+        self.queue = Queue()
 
-        cache_path = self.config.get("yator", "cache", "path", "")
-        structure_path = self.config.get("yator", "structure", "path", "")
+        self.cache_path = self.config.get("yator", "cache", "path", "")
+        self.structure_path = self.config.get("yator", "structure", "path", "")
 
-        if not os.path.isdir(cache_path):
-            os.makedirs(cache_path, 0755)
+        self.library = Library(self.config.get("itunes", "", "library", ""))
 
-        if not os.path.isdir(structure_path):
-            os.makedirs(structure_path, 0755)
+        self.queue_prepare()
+        self.queue_worker()
+        self.song_rebase()
+
+
+    def song_rebase(self):
+        if not os.path.isdir(self.cache_path):
+            os.makedirs(self.cache_path, 0755)
+
+        if not os.path.isdir(self.structure_path):
+            os.makedirs(self.structure_path, 0755)
 
         # Делаем минимальную структуру для работы сд ченжера
         #
@@ -43,23 +52,22 @@ class Yator:
             if len(str(cd_index)) == 1:
                 cd_index = "0{0}".format(cd_index)
 
-            cd_path = "{0}CD{1}".format(structure_path, str(cd_index))
+            cd_path = "{0}CD{1}".format(self.structure_path, str(cd_index))
 
             if not os.path.isdir(cd_path):
                 os.makedirs(cd_path, 0755)
 
-        library = Library(self.config.get("itunes", "", "library", ""))
-        playlists = library.getPlaylistNames()
+        playlists = self.library.getPlaylistNames()
 
         for playlist in playlists:
             playlist_match = self.playlist_re.search(playlist)
 
             if playlist_match:
                 disk_number = playlist_match.group('disk')
-                disk_folder = "{0}CD{1}/".format(structure_path, disk_number)
+                disk_folder = "{0}CD{1}/".format(self.structure_path, disk_number)
 
-                print "Find Yator playlist \'{0}\' at disk {1} with {2} songs".format(playlist, disk_number,
-                                                                        len(library.getPlaylist(playlist).tracks))
+                print "Заполняем структуру по плейлисту \'{0}\' как диск {1} с {2} композицией(ями)".format(playlist,
+                                                        disk_number, len(self.library.getPlaylist(playlist).tracks))
 
                 if not os.path.isdir(disk_folder):
                     os.makedirs(disk_folder, 0755)
@@ -68,20 +76,55 @@ class Yator:
                     for f in filelist:
                         os.remove("{0}/{1}".format(disk_folder, f))
 
-                for song in library.getPlaylist(playlist).tracks:
-                    print "    Prepare #{2} [{0} - {1}]".format(song.artist, song.name, song.playlist_order)
+                for song in self.library.getPlaylist(playlist).tracks:
+                    print "    Перемещаем #{2} [{0} - {1}]".format(song.artist, song.name, song.playlist_order)
 
-                    file_cache = "{0}{1}.mp3".format(cache_path, song.track_id)
+                    file_cache = "{0}{1}.mp3".format(self.cache_path, song.track_id)
                     file_name = self.make_safe_filename(
                                              translit("{0}_{1} - {2}.mp3".format(song.playlist_order, song.artist,
                                                                                  song.name),
                                                       language_code="ru", reversed=True))
                     file_structure = "{0}{1}".format(disk_folder, file_name)
 
-                    if not os.path.exists(file_cache):
-                        transcode.transcode("/{0}".format(song.location), file_cache, bitrate=256)
-
                     shutil.copyfile(file_cache, file_structure)
+
+    def queue_prepare(self):
+        print 'Подготавливаем очередь для транскодинга...'
+
+        self.library = Library(self.config.get("itunes", "", "library", ""))
+        playlists = self.library.getPlaylistNames()
+
+        for playlist in playlists:
+            playlist_match = self.playlist_re.search(playlist)
+
+            if playlist_match:
+                print "Нашли плейлист \'{0}\' с {1} композицией(ями)".format(playlist,
+                                                                        len(self.library.getPlaylist(playlist).tracks))
+
+                for song in self.library.getPlaylist(playlist).tracks:
+                    self.queue.put(["/{0}".format(song.location), "{0}{1}.mp3".format(self.cache_path, song.track_id)])
+
+    def queue_worker(self):
+        thread_count = self.config.get("transcode", "", "threads", "2")
+
+        print 'Зупускаем {0} транскодеров(а).'.format(thread_count)
+
+        for thread_index in xrange(0, int(thread_count), 1):
+            worker = Thread(target=self.thread_worker, args=(thread_index, self.queue,))
+            worker.setDaemon(True)
+            worker.start()
+
+        self.queue.join()
+        print 'Транскодинг завершен.'
+
+    def thread_worker(self, thread_index, queue):
+        transcode = AudioTranscode()
+
+        while True:
+            [file_orig, file_transcoded] = queue.get()
+            print '{0}: {1}'.format(thread_index, file_orig)
+            transcode.transcode(file_orig, file_transcoded, bitrate=256)
+            queue.task_done()
 
     @staticmethod
     def make_safe_filename(filename):
